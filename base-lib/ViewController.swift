f682204cde86da4efc75326a8cffc20ac3cbb498
heliofeliciano <heliofeliciano@gmail.com>
2023-02-14 11:29:43 -0300

    first commit

---
+++ base-lib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  base-lib
+//
+//  Created by Helio Feliciano on 14/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
